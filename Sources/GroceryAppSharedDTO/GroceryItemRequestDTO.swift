e5051a20f24894ea790f657e2027b159c88a063d
Dawei Hao <dwsamurai84@gmail.com>
2025-12-09 23:58:57 +0800

    Add GroceryItemRequestDTO & GroceryItemResponseDTO

---
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Dawei Hao on 2025/12/9.
+//
+
+import Foundation
+
+public struct GroceryItemRequestDTO: Codable {
+  public var title: String
+  public var price: Double
+  public var quantity: Int
+  
+  public init(title: String, price: Double, quantity: Int) {
+    self.title = title
+    self.price = price
+    self.quantity = quantity
+  }
+}
---
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Dawei Hao on 2025/12/9.
+//
+
+import Foundation
+
+public struct GroceryItemResponseDTO: Codable {
+  public var id: UUID
+  public var title: String
+  public var price: Double
+  public var quantity: Int
+  
+  public init(id: UUID, title: String, price: Double, quantity: Int) {
+    self.id = id
+    self.title = title
+    self.price = price
+    self.quantity = quantity
+  }
+}
